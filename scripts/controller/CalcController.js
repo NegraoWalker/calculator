13297fe5687e6e720d9feffe82b5646376b06702
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-06-19 19:39:41 -0300

    Criação do método inicialize

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,23 +1,33 @@
 class CalcController { //Representação das regras de negócio do projeto
     constructor() { //Método construtor definido para a classe
         this._displayCalc = "0" // O uso do _ antes do nome da propriedade é usado para altera-lá para private.
-        this._dataAtual
+        this._currentDate
+        this.initialize()
     }
 
+    initialize() { // Método principal que será usado para iniciar a calculadora
+        let displayCalEl = document.querySelector("#display")
+        let dateEl = document.querySelector("#data")
+        let timeEl = document.querySelector("#hora")
+
+        displayCalEl.innerHTML = 1000
+        dateEl.innerHTML = "19/06/2022"
+        timeEl.innerHTML = "19:36"
+    }
     get displayCalc() {
         return this._displayCalc
     }
 
-    set displayCalc(valor) {
-        this._displayCalc = valor
+    set displayCalc(value) {
+        this._displayCalc = value
     }
 
-    get dataAtual() {
-        return this._dataAtual
+    get currentDate() {
+        return this._currentDate
     }
 
-    set dataAtual(valor) {
-        this._dataAtual = valor
+    set currentDate(value) {
+        this._currentDate = value
     }
 
 }
